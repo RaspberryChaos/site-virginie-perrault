2383ad8d21de4f82f0a7300a54d4d91274aab8ed
Ailie McCorkindale <ailiem134@hotmail.com>
2021-08-29 16:50:04 +0200

    atelier portage en echarpe removed. Javascript cleaned up

--- app.js
+++ app.js
@@ -1,52 +1,22 @@
-//Mobile Nav-Menu
-
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const navLink = document.querySelectorAll(".nav-link");
-
-hamburger.addEventListener("click", mobileMenu);
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
-
-function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}
-
-window.addEventListener("resize", closeMenu);
-
 //Prestations - "En Savoir Plus" buttons
+const espButtons = document.querySelectorAll(".more-info");
 
-const btnMB = document.getElementById("esp-massage-bebe");
-const btnPE = document.getElementById("esp-portage-echarpe");
-const btnGPP = document.getElementById("esp-gpp");
-const btnME = document.getElementById("esp-me");
-
-btnMB.addEventListener("click", (e) => {
-    e.target.textContent = e.target.textContent === "Voir Moins" ? "En Savoir Plus" : "Voir Moins";
-    document.getElementById("long-mb").classList.toggle("hidden");
+espButtons.forEach(btn => {
+    btn.addEventListener("click", e => enSavoirPlus(e));
 });
 
-btnPE.addEventListener("click", (e) => {
+function enSavoirPlus(e) {
     e.target.textContent = e.target.textContent === "Voir Moins" ? "En Savoir Plus" : "Voir Moins";
-    document.getElementById("short-pe").classList.toggle("hidden");
-    document.getElementById("long-pe").classList.toggle("hidden");
-});
-
-btnGPP.addEventListener("click", (e) => {
-    e.target.textContent = e.target.textContent === "Voir Moins" ? "En Savoir Plus" : "Voir Moins";
-    document.getElementById("short-gpp").classList.toggle("hidden");
-    document.getElementById("long-gpp").classList.toggle("hidden");
-});
-
-btnME.addEventListener("click", (e) => {
-    e.target.textContent = e.target.textContent === "Voir Moins" ? "En Savoir Plus" : "Voir Moins";
-    document.getElementById("long-me").classList.toggle("hidden");
-});
+    
+    const value = e.target.value;
+    
+    if(value === "groupe_parole_papa") {
+        document.getElementById("short-gpp").classList.toggle("hidden");
+        document.getElementById("long-gpp").classList.toggle("hidden");
+    } else {
+        document.getElementById(`long-${value}`).classList.toggle("hidden");
+    }   
+}
 
 
 
